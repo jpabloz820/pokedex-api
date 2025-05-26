f5c144f490893798bb9c291fd529efe110ce7c01
jpabloz820 <juampa.820@gmail.com>
2025-05-26 13:57:51 -0500

    Add Cors

--- pokedex-api/src/main/java/com/pokedex/pokedex_api/config/SecurityConfig.java
+++ pokedex-api/src/main/java/com/pokedex/pokedex_api/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.pokedex.pokedex_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.pokedex.pokedex_api.services.UserDetailsServiceImpl;
@@ -40,6 +43,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/login").permitAll() //Ruta personalizada del login
@@ -52,5 +56,17 @@ public class SecurityConfig {
                 .authenticationProvider(authenticationProvider())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:4200"); // Tu frontend
+        configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE, etc.
+        configuration.addAllowedHeader("*"); // Headers como Content-Type, Authorization
+        configuration.setAllowCredentials(true); // Si usas cookies o auth básica
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/", configuration);
+        return source;
     }
 }
